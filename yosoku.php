c97652bcc1938dd41bfa80399d599227c1f575f3
h494yosh <h520494b@mails.cc.ehime-u.ac.jp>
2022-12-03 14:09:42 +0900

    csv

--- yosoku.php
+++ yosoku.php
@@ -1,3 +1,9 @@
 <?php
+$time1 = new DateTime('2020/02/22 09:23:00');
+$time2 = new DateTime('2020/03/10 09:23:00');
 
-?>
+$diff = $time1->diff($time2);
+
+if ($diff->h == 0 && $diff->i == 0 && $diff->s == 0) {
+  echo '一致しています';
+}
