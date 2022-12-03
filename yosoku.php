73d5370029e215ce3330b0934b1ab495e2cf4602
h494yosh <h520494b@mails.cc.ehime-u.ac.jp>
2022-12-03 15:55:44 +0900

    締め日、深夜手当の考慮なし。 まず、csv内の aさんのスケジュール全部を計算するようになっている。

--- yosoku.php
+++ yosoku.php
@@ -1,9 +1,30 @@
 <?php
-$time1 = new DateTime('2020/02/22 09:23:00');
-$time2 = new DateTime('2020/03/10 09:23:00');
+$fp = fopen('job_schedule.csv', 'r');
+$num_sche = 0;
 
-$diff = $time1->diff($time2);
-
-if ($diff->h == 0 && $diff->i == 0 && $diff->s == 0) {
-  echo '一致しています';
+while ($line = fgetcsv($fp)) {
+  if ($line[0] == 'a') {
+    $arr_sche[$num_sche] = $line;
+    $num_sche = $num_sche + 1;
+  }
 }
+fclose($fp);
+//print_r($arr_sche);
+// echo '<br>'.$num_sche;
+for ($i = 0; $i < $num_sche; $i++) {
+  $from_time = strtotime($arr_sche[$i][3]);
+  $to_time = strtotime($arr_sche[$i][4]);
+  $diff = $to_time - $from_time;
+  $min_time = $diff/60;        //勤務時間（分）
+  $Hour_wage = $arr_sche[$i][5];
+  $min_wage = $Hour_wage / 60;  //分給
+  $day_salary = $min_wage * $min_time;
+  $salary += $day_salary; 
+}
+echo $salary;
+
+// $diff = $time1->diff($time2);
+
+// if ($diff->h == 0 && $diff->i == 0 && $diff->s == 0) {
+//   echo '一致しています';
+// }
