c5900449f36be8ea118e0fbd764c843c6c6d8d38
h410miur <108775082+h410miur@users.noreply.github.com>
2022-12-05 15:17:44 +0900

    Update add.php

--- add.php
+++ add.php
@@ -1,23 +1,41 @@
-<meta charset="UTF-8">
-<h1>新規登録</h1>
-<form action="check.php" method="post" name="info">
-  <table>
-    <tr class="">
-      <p class="form_caution">全項目が必須入力です。</p>
-    </tr>
-    <tr class="">
-      <th><label for="id">ID</label></th>
-      <td><input type="text" name="id" required></td>
-    </tr>
-    <tr class="">
-      <th><label for="pass">パスワード</label></th>
-      <td><input type="password" name="pass" required></td>
-    </tr>
-  </table>
-  <form action="check.php" method="post" >
-    <div>
-      <input type="submit" value="登録">
-    </div>
+<?php
+session_start();
+unset($_SESSION['user_id']);
+unset($_SESSION['pass']);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>新規登録</h1>
+  <form action="check.php" method="post" name="info">
+    <table>
+      <tr class="">
+        <p class="form_caution">全項目が必須入力です。</p>
+      </tr>
+      <tr class="">
+        <td><p><input type="text" placeholder="ユーザー名" name="user_id" required></p></td>
+      </tr>
+      <tr class="">
+        <td><input type="password" placeholder="パスワード" name="pass" required></p></td>
+      </tr>
+    </table>
+    <form action="check.php" method="post">
+      <div>
+        <input type="submit" value="登録">
+      </div>
+    </form>
   </form>
-</form>
-<p><a href="http://localhost/PBL2/index.php">home</a></p>
+  <p><a href="index.php">TOP</a></p>
+</body>
+
+</html>
