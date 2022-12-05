1b9da15e8b187e5781449b77310615842cc6ae1b
h410miur <108775082+h410miur@users.noreply.github.com>
2022-12-05 15:16:56 +0900

    Update check.php

--- check.php
+++ check.php
@@ -1,30 +1,43 @@
 <?php
-$id = $_POST['id'];
+$user_id = $_POST['user_id'];
 $pass = $_POST['pass'];
-session_start();//セッションスタート
-$_SESSION['id'] = $id;
-$_SESSION['pass'] = $pass;
-$data = file("id.txt");
-$key = '長い鍵長い鍵長い鍵長い鍵長い鍵長い鍵長い鍵長い鍵長い鍵長い鍵';
-$c_t = openssl_encrypt($_SESSION['pass'], 'AES-128-ECB', $key);
-$p_t = openssl_decrypt($c_t, 'AES-128-ECB', $key);
-$_SESSION['c_t'] = $c_t;
-
-foreach ($data as $line) {
-
-  if (rtrim($line) == $id) {
-    echo '入力されたIDは既に利用されています。';
-    echo '<p><a href="http://localhost/PBL2/index.php">home</a></p>';
-    echo '<p><a href="http://localhost/PBL2/add.php">新規登録画面</a></p>';
-    exit();
-  }
-  if (mb_strlen($_POST['pass']) < 8) {
-    echo '8文字以上のパスワードを入力してください。';
-    echo '<p><a href="http://localhost/PBL2/index.php">home</a></p>';
-    echo '<p><a href="http://localhost/PBL2/add.php">新規登録画面</a></p>';
-    exit();
-  }
-
-}
-header("Location:http://localhost/PBL2/add1.php");
+session_start(); //セッションスタート
+$_SESSION['user_id'] = $user_id;
+$_SESSION['pass'] =  $pass;
+$db = new PDO('sqlite:part-time-job.db');
+$result = $db->query("select count(*) from user_inf where user_id = '$user_id'");
+$count = $result->fetchColumn();
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  if($count == 1) {
+    echo '<p>入力されたIDは既に利用されています。</p>';
+    echo '<p><a href="index.php">home</a></p>';
+    echo '<p><a href="add.php">新規登録画面</a></p>';
+    exit();
+  }
+
+  if (mb_strlen($_POST['pass']) < 8) {
+    echo '<p>8文字以上のパスワードを入力してください。</p>';
+    echo '<p><a href="index.php">home</a></p>';
+    echo '<p><a href="add.php">新規登録画面</a></p>';
+    exit();
+  }
+
+  header("Location:add1.php");
+  ?>
+</body>
+
+</html>
