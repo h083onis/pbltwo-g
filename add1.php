c8505c0ea7e5713ce3e1fa945378503f05ea15e2
h410miur <108775082+h410miur@users.noreply.github.com>
2022-12-05 15:16:03 +0900

    Update add1.php

--- add1.php
+++ add1.php
@@ -1,18 +1,32 @@
-<meta charset="UTF-8">
-<h1>登録完了しました。</h1>
 <?php
 session_start();
-$id = $_SESSION['id'];
-$pass = $_SESSION['c_t'];
-$write_test = fopen("test.txt" , "a");
-if(flock($write_test , LOCK_EX)){
-  fwrite($write_test , $id . "," . $pass . "\n");
+$user_id = $_SESSION['user_id'];
+$pass = $_SESSION['pass'];
+$encryptedPw = password_hash($pass, PASSWORD_DEFAULT);
+$db = new PDO('sqlite:part-time-job.db');
+$sql = "insert into user_inf(user_id, pass) values(:user_id, :pass)"; //idはint型として代入
+if ($stmt = $db->prepare($sql)) {
+  $stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
+  $stmt->bindValue(':pass', $encryptedPw, PDO::PARAM_STR);
+  $stmt->execute();
 }
-fclose($write_test);
-$write_id = fopen("id.txt" , "a");
-if(flock($write_id , LOCK_EX)){
-  fwrite($write_id , $id . "\n");
-}
-fclose($write_id);
+$db = null;
 ?>
-<p><a href="http://localhost/PBL2/index.php">home</a></p>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>登録完了しました。</h1>
+  <p><a href="index.php">home</a></p>
+</body>
+
+</html>
