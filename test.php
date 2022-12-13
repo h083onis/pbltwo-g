179c9eed9b62254b1abc2bc36a55954ae23a8ebc
onishi <h520083m@mails.cc.ehime-u.ac.jp>
2022-12-13 13:19:50 +0900

    テスト

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $user_id = 1;
 $db = new PDO("sqlite:part-time-job.db");
 $job_name = 'コンビニ';
 $result1 = $db->query("select * from part_time_job_inf where user_id = '$user_id' and job_name = '$job_name'");
-$job_inf = $result->feachAll();
+$job_inf = $result1->feachAll();
 echo $job_inf['user_id'];
 
 
