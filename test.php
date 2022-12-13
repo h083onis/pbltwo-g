ccb42ac8175f6796b0b801c2c1b7a3f422083681
onishi <h520083m@mails.cc.ehime-u.ac.jp>
2022-12-13 13:29:11 +0900

    テスト

--- test.php
+++ test.php
@@ -4,9 +4,14 @@ $user_id = 1;
 $db = new PDO("sqlite:part-time-job.db");
 $job_name = 'コンビニ';
 $result1 = $db->query("select * from part_time_job_inf where user_id = '$user_id' and job_name = '$job_name'");
-$job_inf = $result1->feachAll();
-echo $job_inf['user_id'];
-
+foreach($result1 as $value){
+    $houryly_wage = $value['hourly_wage'];
+    $cutoff_day = $value['cutoff_day'];
+    $mid_wage = $value['cutoff_day'];
+    $start_mid_time = $value['start_mid_time'];
+    $end_mid_time = $value['end_mid_time'];
+}
+echo  $houryly_wage;
 
 // $result2 = $db->query("select * job_schedule where date > ")
 ?>
