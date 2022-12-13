e1e4252771b18ed4acefc88ede4567df4d4bed3e
onishi <h520083m@mails.cc.ehime-u.ac.jp>
2022-12-13 13:50:58 +0900

    テスト

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
 // $user_id = $_SESSINN['user_id'];
 $user_id = 1;
+$
 $db = new PDO("sqlite:part-time-job.db");
 $job_name = '居酒屋';
 $result1 = $db->query("select * from part_time_job_inf where user_id = '$user_id' and job_name = '$job_name'");
@@ -13,5 +14,20 @@ foreach($result1 as $value){
 }
 echo  $start_mid_time;
 
+$y = 2022;
+$m = 12;
+$tem_m = $m - 1;
+$tem_y = $y;
+if($tem == 0){
+    $tem_m = 12;
+    $ten_y = $y-1;
+}
+
+$pre_job_date = strval($tem_y) . '-' . strval($tem_m) . '-' . strval($cutoff_day);
+$now_job_date = strval($y) . '-' . strval($m) . '-' . strval($cutoff_day);
+$result2 = $db->query("select * from job_schedule where user_id ='$user_id' and job_name ='$job_name' and job_date > '$pre_job_date' and job_date <= '$now_job_date'");
+foreach($result2 as $value){
+    echo $value['start_time'].'<br>';
+}
 // $result2 = $db->query("select * job_schedule where date > ")
 ?>
