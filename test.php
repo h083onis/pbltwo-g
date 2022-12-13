edbd37e0e326b4fecb6fb59fd5f31b3131bbec42
onishi <h520083m@mails.cc.ehime-u.ac.jp>
2022-12-13 13:18:41 +0900

    テスト

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+// $user_id = $_SESSINN['user_id'];
+$user_id = 1;
+$db = new PDO("sqlite:part-time-job.db");
+$job_name = 'コンビニ';
+$result1 = $db->query("select * from part_time_job_inf where user_id = '$user_id' and job_name = '$job_name'");
+$job_inf = $result->feachAll();
+echo $job_inf['user_id'];
+
+
+// $result2 = $db->query("select * job_schedule where date > ")
+?>
