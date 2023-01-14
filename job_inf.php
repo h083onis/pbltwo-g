e73b18b25248012fc7fdb38aca188e5cc677648a
mori <h520437a@mails.cc.ehime-u.ac.jp>
2023-01-14 15:36:40 +0900

    デザイン変更

--- job_inf.php
+++ job_inf.php
@@ -163,12 +163,12 @@ $db = null;
         <form action='edit_inf.php' method='post'>
           <?php
           foreach ($result2 as $value) {
-            echo 'バイト名<input type=\'text\' name=\'job_name\'value=', $value['job_name'], '>';
-            echo '時給入力<input type=\'number\' name=\'hourly_wage\' min=\'0\' value=', $value['hourly_wage'], ' required><br>';
-            echo '締め日<input type=\'number\' name=\'cutoff_day\' min=\'1\' max=\'31\' value=', $value['cutoff_day'], ' required><br>';
-            echo '給料日<input type=\'number\' name=\'payment_day\' min=\'1\' max=\'31\' value=', $value['payment_day'], ' required><br>';
-            echo '深夜手当時給入力<input type=\'number\' name=\'mid_wage\' min=\'0\' value=', $value['mid_wage'], '><br>';
-            echo '深夜手当時間';
+            echo 'バイト名：<input type=\'text\' name=\'job_name\'value=', $value['job_name'], '><br>';
+            echo '時給：<input type=\'number\' name=\'hourly_wage\' min=\'0\' value=', $value['hourly_wage'], ' required><br>';
+            echo '締め日：<input type=\'number\' name=\'cutoff_day\' min=\'1\' max=\'31\' value=', $value['cutoff_day'], ' required><br>';
+            echo '給料日：<input type=\'number\' name=\'payment_day\' min=\'1\' max=\'31\' value=', $value['payment_day'], ' required><br>';
+            echo '深夜手当時給：<input type=\'number\' name=\'mid_wage\' min=\'0\' value=', $value['mid_wage'], '><br>';
+            echo '深夜手当時間：';
             echo '<input type=\'time\' name=\'start_mid_time\' style=\'width:80px\' step=\'60\' value=', $value['start_mid_time'], '>~';
             echo '<input type=\'time\' name=\'end_mid_time\' style=\'width:80px\' step=\'60\' value=', $value['end_mid_time'], '>';
             echo '<input type=\'hidden\' name=\'pre_name\' value=', $value['job_name'], '>';
