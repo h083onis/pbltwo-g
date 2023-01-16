5f05efb178b5e2e275c67dd977d2a56046826982
mori <h520437a@mails.cc.ehime-u.ac.jp>
2023-01-16 12:01:16 +0900

    デザイン変更

--- job_inf.php
+++ job_inf.php
@@ -212,8 +212,8 @@ $db = null;
           <input type='password' name='first_pass' minlength='8' required></br>
           <input type='password' name='second_pass' minlength='8' required> 
         </form>
-        <input type='button' value='やめる' onclick="close_popup3()"><br>
-        <input type='submit' value='変更' form='edit_pass'>
+        <input type='button' value='やめる' onclick="close_popup3()" class="button">
+        <input type='submit' value='変更' form='edit_pass' class="button">
       </div>
     </div>
     </div>
