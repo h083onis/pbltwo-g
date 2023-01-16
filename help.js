4f99c3f9899a7f88f7df8e9e206a2f27902d10ff
h410miur <108775082+h410miur@users.noreply.github.com>
2023-01-16 14:52:35 +0900

    Add files via upload

---
+++ help.js
@@ -0,0 +1,44 @@
+$(function() {
+    $(".A").click(function() {
+        $(".B").slideToggle("Z");
+    });
+});
+$(function() {
+    $(".C").click(function() {
+        $(".D").slideToggle("Z");
+    });
+});
+$(function() {
+    $(".E").click(function() {
+        $(".F").slideToggle("Z");
+    });
+});
+$(function() {
+    $(".G").click(function() {
+        $(".H").slideToggle("Zs");
+    });
+});
+$(function() {
+    $(".I").click(function() {
+        $(".J").slideToggle("Zs");
+    });
+});
+$(function() {
+    $(".K").click(function() {
+        $(".L").slideToggle("Zs");
+    });
+});
+$(function() {
+    $(".M").click(function() {
+        $(".N").slideToggle("Zs");
+    });
+});
+$(function() {
+    $(".O").click(function() {
+        $(".P").slideToggle("Zs");
+    });
+});$(function() {
+    $(".Q").click(function() {
+        $(".R").slideToggle("Zs");
+    });
+});
---
+++ help.php
@@ -0,0 +1,39 @@
+<meta charset="UTF-8">
+<link rel="stylesheet" href="help.css">
+<script src="https://code.jquery.com/jquery.min.js"></script>
+<script src="help.js">
+</script>
+
+<h1>HELP</h1>
+<br>
+<table>
+  <tr class="">
+    <th><p>よくある質問</p></th>
+  </tr>
+  <tr class="">
+    <td>
+      <b>初期設定について</b><br>
+      <div class="A">&#x25B6; 質問1</div><br><p></p>
+      <div class="B">回答1</div><p></p>
+      <div class="C">&#x25B6; 質問2</div><br><p></p>
+      <div class="D">回答2</div><p></p>
+      <div class="E">&#x25B6; 質問3</div><br><p></p>
+      <div class="F">回答3</div><p></p>
+      <b>カレンダー機能について</b><br>
+      <div class="G">&#x25B6; カレンダーにシフトを追加する</div><br><p></p>
+      <div class="H">回答1</div><p></p>
+      <div class="I">&#x25B6; カレンダーのシフトを削除する</div><br><p></p>
+      <div class="J">回答2</div><p></p>
+      <div class="K">&#x25B6; 質問3</div><br><p></p>
+      <div class="L">回答3</div><p></p>
+      <b>給与計算について</b><br>
+      <div class="M">&#x25B6; 質問1</div><br><p></p>
+      <div class="N">回答1</div><p></p>
+      <div class="O">&#x25B6; 質問2</div><br><p></p>
+      <div class="P">回答2</div><p></p>
+      <div class="Q">&#x25B6; 質問3</div><br><p></p>
+      <div class="R">回答3</div><p></p>
+    </table>
+    <?php
+    echo '<p><a href="home.php">home</a></p>';
+    ?>
