627954690e65f41cb93750bf72e3e7c0a1800187
h342hama <108774428+h342hama@users.noreply.github.com>
2023-01-23 11:44:30 +0900

    Update help.php

--- help.php
+++ help.php
@@ -12,27 +12,14 @@
   </tr>
   <tr class="">
     <td>
-      <b>初期設定について</b><br>
-      <div class="A">&#x25B6; 質問1</div><br><p></p>
-      <div class="B">回答1</div><p></p>
-      <div class="C">&#x25B6; 質問2</div><br><p></p>
-      <div class="D">回答2</div><p></p>
-      <div class="E">&#x25B6; 質問3</div><br><p></p>
-      <div class="F">回答3</div><p></p>
+      <b>タスクバーについて</b><br>
+      <div class="A">&#x25B6; それぞれのアイコンの説明</div><br><p></p>
+      <div class="B">上から「個人情報ページ」「シフト編集ページ」「給与計算・グラフページ」「ヘルプページ」「ログアウト」となっています</div><p></p>
       <b>カレンダー機能について</b><br>
       <div class="G">&#x25B6; カレンダーにシフトを追加する</div><br><p></p>
-      <div class="H">回答1</div><p></p>
+      <div class="H">カレンダー上の該当する日付をクリックすることで、シフト追加のポップアップが開きます。</div><p></p>
       <div class="I">&#x25B6; カレンダーのシフトを削除する</div><br><p></p>
-      <div class="J">回答2</div><p></p>
-      <div class="K">&#x25B6; 質問3</div><br><p></p>
-      <div class="L">回答3</div><p></p>
-      <b>給与計算について</b><br>
-      <div class="M">&#x25B6; 質問1</div><br><p></p>
-      <div class="N">回答1</div><p></p>
-      <div class="O">&#x25B6; 質問2</div><br><p></p>
-      <div class="P">回答2</div><p></p>
-      <div class="Q">&#x25B6; 質問3</div><br><p></p>
-      <div class="R">回答3</div><p></p>
+      <div class="J">カレンダー上の該当する日付をクリックすることでポップアップが表示され、そこに既に追加しているシフト情報が表示されます。該当するシフト情報の右端にある「×」ボタンをクリックすることでシフト情報を削除することができます。</div><p></p>
     </table>
     <?php
     echo '<p><a href="home.php">home</a></p>';
