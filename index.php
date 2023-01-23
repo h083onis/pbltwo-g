99ab95ea76649494823b73282407871b0e277b93
mori <h520437a@mails.cc.ehime-u.ac.jp>
2023-01-23 13:34:33 +0900

    デザイン変更

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ unset($_SESSION['pass']);
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="index.css">
-	<title>Document</title>
+	<title>smart-バイト管理アプリ</title>
 </head>
 
 <body class="picture">
