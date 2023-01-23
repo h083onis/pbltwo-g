7c1fd9754efca082373f8bfe87a505956ac0275b
h342hama <108774428+h342hama@users.noreply.github.com>
2023-01-23 11:50:17 +0900

    Create help.php

---
+++ help.php
@@ -0,0 +1,26 @@
+<meta charset="UTF-8">
+<link rel="stylesheet" href="help.css">
+<script src="https://code.jquery.com/jquery.min.js"></script>
+<script src="help.js">
+</script>
+
+<h1>HELP</h1>
+<br>
+<table>
+  <tr class="">
+    <th><p>よくある質問</p></th>
+  </tr>
+  <tr class="">
+    <td>
+      <b>タスクバーについて</b><br>
+      <div class="A">&#x25B6; それぞれのアイコンの説明</div><br><p></p>
+      <div class="B">上から「個人情報ページ」「シフト編集ページ」「給与計算・グラフページ」「ヘルプページ」「ログアウト」となっています</div><p></p>
+      <b>カレンダー機能について</b><br>
+      <div class="G">&#x25B6; カレンダーにシフトを追加する</div><br><p></p>
+      <div class="H">カレンダー上の該当する日付をクリックすることで、シフト追加のポップアップが開きます。</div><p></p>
+      <div class="I">&#x25B6; カレンダーのシフトを削除する</div><br><p></p>
+      <div class="J">カレンダー上の該当する日付をクリックすることでポップアップが表示され、そこに既に追加しているシフト情報が表示されます。該当するシフト情報の右端にある「×」ボタンをクリックすることでシフト情報を削除することができます。</div><p></p>
+    </table>
+    <?php
+    echo '<p><a href="home.php">home</a></p>';
+    ?>
